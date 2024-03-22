b621b4eb54ab269239db91b0791f6b6ed193bda6
kimsm0566 <kimsm0566@naver.com>
2024-03-23 06:32:34 +0900

    0322

--- queue/2164_queue_linkedlist.c
+++ queue/2164_queue_linkedlist.c
@@ -1,5 +1,3 @@
-//이중연결리스트로 작성해야할듯
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,145 +5,72 @@
 
 struct NODE{
     int key;
-    struct NODE* next;
+    struct NODE* next_node;
 };
 
-void Make_LastNode(struct NODE ** head,int n);
-void deleteFirstNode(struct NODE ** head);
-void lastTofirst(struct NODE **head);
+struct QUEUE{
+    struct NODE* front;
+    struct NODE* rear;
+};
+
+void Make_LastNode(struct QUEUE*,int );
+int deleteFirstNode(struct QUEUE* );
 
 int main(){
 
-    int n;
 
+    int n;
     scanf("%d",&n);
 
-    struct NODE* node = (struct NODE*)malloc(sizeof(struct NODE));
-    node->key=0;
-    node->next=NULL;
+    struct QUEUE * queue = (struct QUEUE*)malloc(sizeof(struct QUEUE));
+    struct NODE * head = (struct NODE*)malloc(sizeof(struct NODE));
+
+
+    queue->front = head;
+    queue->rear = head;
+
+    head->key = 0;
+    head->next_node = NULL;
 
     for (int i=0;i<n;i++){
-        Make_LastNode(&node,i+1);
+        Make_LastNode(queue,i+1);
     }
-    node = node->next; //맨 앞의 빈 노드 지우기
-
-    // for (int i=0;node->next != NULL;i++){
-    //     deleteFirstNode(&node);
-    //     lastTofirst(&node);
-    // }
-    while(node->next){
-        deleteFirstNode(&node);
-        lastTofirst(&node); 
-    }
-
-    printf("%d\n",node->key);
-
-}
-
-void Make_LastNode(struct NODE **head,int n){//맨 마지막 노드 만들고 값넣기
-
-    struct NODE * ptr = (struct NODE*)malloc(sizeof(struct NODE));
-    struct NODE * trv;
-    trv = *head;
-    ptr->key = n;
-    ptr->next = NULL;
-
-    while(trv->next){
-        trv = trv->next;
-    }
-    trv->next = ptr;
-}
-
-void deleteFirstNode(struct NODE ** head){ //맨 앞의 노드 지우기
-    struct NODE *ptr = *head;
-    struct NODE *next_ptr = *head;
-    next_ptr = next_ptr->next;
-    ptr->key=0;
-    ptr->next = NULL;
-    *head = next_ptr;
-}
-
-void lastTofirst(struct NODE **head){ //맨 앞의 값 뒤으로 옮기고 앞 노드 지우기
-    struct NODE *ptr = *head;
-
-    Make_LastNode(head,ptr->key);
-    deleteFirstNode(head);
-}
-
-// #include <stdio.h>
-// #include <stdlib.h>
-
-// #define MAXBUF 1000000
-
-// struct NODE{
-//     int key;
-//     struct NODE* next;
-// };
-
-// void Make_FirstNode(struct NODE ** head);
-// void deleteNode(struct NODE ** head);
-// void lastTofirst(struct NODE **head);
-
-// int main(){
-
-//     int n;
-
-//     scanf("%d",&n);
-
-//     struct NODE* node = (struct NODE*)malloc(sizeof(struct NODE));
-//     node->key=0;
-//     node->next=NULL;
-
-//     for (int i=0;i<n;i++){
-//         node->key = i+1;
-//         if (i+1 != n) Make_FirstNode(&node);
-//     }
-
-
-//     for (int i=0;node->next != NULL;i++){
-//         deleteNode(&node);
-//         lastTofirst(&node);
-
-//     }
-
-//     printf("%d\n",node->key);
-
-// }
-
-// void Make_FirstNode(struct NODE **head){
-
-//     struct NODE *ptr = (struct NODE*)malloc(sizeof(struct NODE));
-//     ptr->key=0;
-//     ptr->next = *head;
-//     *head = ptr;
-// }
-
-// void deleteNode(struct NODE ** head){ //맨 뒤의 노드 지우기
-//     struct NODE *ptr = *head;
-//     struct NODE *prv_ptr = *head;
-
-//     ptr = ptr->next;
-
-//     while(ptr->next){
-//         prv_ptr = prv_ptr->next;
-
-//         ptr=ptr->next;
-
-//     }
-//     prv_ptr->next = NULL;
-//     free(ptr);
-// }
-
-// void lastTofirst(struct NODE **head){
-//     struct NODE *ptr = *head;
     
-//     while(ptr->next){
-//         ptr=ptr->next;
-//     }
-//     Make_FirstNode(head);
-//     struct NODE *first_ptr = *head;
+    head = head->next_node;
+    queue->front = queue->front->next_node;
 
-//     first_ptr->key = ptr->key;
-//     deleteNode(head);
 
-// }
+    while(queue->front->key != queue->rear->key){
+
+        int fir = deleteFirstNode(queue);
+        
+        if(queue->front->key == queue->rear->key) break;
+
+        fir = deleteFirstNode(queue);
+        Make_LastNode(queue,fir);
+
+    }
+    printf("%d\n",queue->front->key);
+
+}
+
+void Make_LastNode(struct QUEUE* queue,int n){
+
+    struct NODE * newNode = (struct NODE*)malloc(sizeof(struct NODE));
+    struct NODE * ptr;
+
+    newNode->key = n;
+    newNode->next_node = NULL;
+
+    queue->rear->next_node = newNode;
+    queue->rear = newNode;
+
+}
+
+int deleteFirstNode(struct QUEUE* queue){
+
+    int front_key = queue->front->key;
+
+    queue->front = queue->front->next_node;
+    return front_key;
+}
---
+++ tree/1991.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Function to create a new tree node
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+// Function to perform inorder traversal
+void inorder(struct Node* root) {
+    if (root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->data);  // Visit 
+        inorder(root->right);
+    }
+}
+
+int main() {
+    struct Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->right = newNode(4);
+    root->right->left = newNode(5);
+
+    printf("Inorder Traversal: ");
+    inorder(root);
+
+    return 0;
+}
